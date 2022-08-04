84568c15bf4390a1e963a8b65a846b484b19072c
Thiago Xavier <thiago.xavier@unidas.com.br>
2022-08-03 22:08:39 -0300

    api

---
+++ api-produto/src/routes/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const express = require('express')
+
+const router = new express.Router();
+
+//endpoint 
+router.get('/', (req, res, next) => {
+    res.status(200).send("Alterar resposta!");
+});
+
+module.exports = router;
